f9dab2888a9fa90249aacdfdf7cabeb32af9c9ad
Victor Hugo Garcia de Farias <vhfarias_1@hotmail.com>
2021-04-23 00:41:20 -0300

    end of day 3

---
+++ src/services/UsersService.ts
@@ -0,0 +1,31 @@
+import { getCustomRepository, Repository } from "typeorm";
+import { User } from "../entities/User";
+import { UsersRepository } from "../repositories/UsersRepository";
+
+class UsersService {
+  private usersRepository: Repository<User>;
+  constructor() {
+    this.usersRepository = getCustomRepository(UsersRepository);
+  }
+
+  async create(email: string) {
+    //Verificar se o Usuário existe
+
+    const userExists = await this.usersRepository.findOne({
+      email
+    })
+
+    //se existir, retornar user
+    if (userExists) {
+      return userExists;
+    }
+    //Se não, salvar no DB
+    const user = this.usersRepository.create({
+      email
+    });
+    await this.usersRepository.save(user);
+    return user;
+  }
+}
+
+export { UsersService }
