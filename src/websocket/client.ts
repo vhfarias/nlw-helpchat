e740d27a353124fc9f7a9501adf4416eee79b90e
Victor Hugo Garcia de Farias <vhfarias_1@hotmail.com>
2021-04-25 03:22:04 -0300

    end of day 5 (last day)

--- src/websocket/client.ts
+++ src/websocket/client.ts
@@ -2,6 +2,7 @@ import { io } from "../http";
 import { ConnectionsService } from "../services/ConnectionsService"
 import { UsersService } from "../services/UsersService"
 import { MessagesService } from "../services/MessagesService"
+import { Socket } from "socket.io";
 
 
 interface IParams {
@@ -55,6 +56,34 @@ io.on("connect", (socket) => {
     //salvar a conexão com o socket_id, user_id
     await messagesService.create({
       text, user_id
-    })
+    });
+
+    const allMessages = await messagesService.listByUser(user_id);
+
+    socket.emit("client_list_all_messages", allMessages);
+
+    const allUsers = await connectionsService.findAllWithoutAdmin();
+    io.emit("admin_list_all_users", allUsers);
   });
+
+  socket.on("client_send_to_admin", async (params) => {
+    const { text, socket_admin_id } = params;
+
+    const socket_id = socket.id;
+
+    const { user_id } = await connectionsService.findBySocketId(socket_id);
+
+    const message = await messagesService.create({
+      text,
+      user_id,
+    });
+
+    io.to(socket_admin_id).emit("admin_receive_message", {
+      message,
+      socket_id,
+    })
+
+  });
+
+
 });
